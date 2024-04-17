3794fc7f3153863a66c5b6230861433e83005341
Michaelc322 <michaelc32204@gmail.com>
2024-04-17 19:25:21 -0400

    cors is not working

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
-      "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+      "GET,POST,DELETE"
     );
     res.setHeader(
       "Access-Control-Allow-Headers",
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Private-Network", true);
     //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
     res.setHeader("Access-Control-Max-Age", 7200);
-  
+
     next();
   });
 
