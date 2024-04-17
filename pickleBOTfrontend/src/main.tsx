3785af6d41d44aa66b6f012e5c50c9531deb534d
Michaelc322 <michaelc32204@gmail.com>
2024-04-17 18:58:08 -0400

    vercel analytics

--- pickleBOTfrontend/src/main.tsx
+++ pickleBOTfrontend/src/main.tsx
@@ -3,9 +3,12 @@ import App from './App.tsx'
 import './index.css'
 import './Styles/fonts.css';
 import { AuthProvider } from '../context/AuthProvider.tsx'; // Import the AuthProvider component from the appropriate module
+import { Analytics } from '@vercel/analytics/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
         <AuthProvider>
+                <Analytics/>
                 <App />
+                
         </AuthProvider>
 )
