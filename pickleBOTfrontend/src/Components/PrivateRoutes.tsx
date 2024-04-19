e7cc210107034516cca0f5b323472823b54dd64b
Michaelc322 <michaelc32204@gmail.com>
2024-04-18 20:27:07 -0400

    is logged in?

--- pickleBOTfrontend/src/Components/PrivateRoutes.tsx
+++ pickleBOTfrontend/src/Components/PrivateRoutes.tsx
@@ -4,6 +4,7 @@ import { useAuth } from '../../context/AuthProvider';
 
 export default function PrivateRoutes() {
     const { isLoggedIn } = useAuth() as any;
+    console.log("isLoggedIn", isLoggedIn)
     return(
         <>
             {isLoggedIn ? <Outlet /> : <Navigate to="/login" /> }
