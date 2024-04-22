a70bf9d43fcd955e5535ce843e165cfa995d2a16
Michaelc322 <michaelc32204@gmail.com>
2024-04-22 00:56:27 -0400

    authy

--- pickleBOTfrontend/context/AuthProvider.tsx
+++ pickleBOTfrontend/context/AuthProvider.tsx
@@ -41,7 +41,7 @@ const [isLoading, setIsLoading] = useState(true);
             console.log("is logged in", isLoggedIn)
             getProfile();
 
-    }, [isLoggedIn])
+    }, [])
 
     const login = () => {
       // Logic to handle login
