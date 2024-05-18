ab386de3b0e709be87067f28b44e6745c8a883ee
Michaelc322 <michaelc32204@gmail.com>
2024-05-17 23:40:11 -0400

    unused declaration

--- pickleBOTfrontend/src/Components/Navbar.tsx
+++ pickleBOTfrontend/src/Components/Navbar.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import { useState } from 'react';
 import { useAuth } from '../../context/AuthProvider.tsx';
 import '../Styles/styles.css';
-import axios from 'axios';
 
 
 
