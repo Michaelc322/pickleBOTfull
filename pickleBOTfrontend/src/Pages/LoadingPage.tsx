cc390b130a45c1e9446e46a14ca0eb7260385970
Michaelc322 <michaelc32204@gmail.com>
2024-05-22 17:36:00 -0400

    loading

--- pickleBOTfrontend/src/Pages/LoadingPage.tsx
+++ pickleBOTfrontend/src/Pages/LoadingPage.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 
 const Section = styled.section`
@@ -8,18 +8,32 @@ const Section = styled.section`
     background-color: #121212;
     height: 100vh;
     flex-direction: column;
-    justify-content: center;
     
 `
 
 
 
+const Spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`
+const Loader = styled.div`
+  border: 4px solid #585858;
+  border-top: 4px solid #c2fd15;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  position: relative;
+  top: 100px;
+  animation: ${Spin} 1s linear infinite;
+
+`
 
 
 const PageNotFound = () => {
   return (
     <Section>
-
+      <Loader/>
     </Section>
   )
 }
